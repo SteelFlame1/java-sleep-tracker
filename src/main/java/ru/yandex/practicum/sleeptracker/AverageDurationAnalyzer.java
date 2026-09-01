e4312e0caa5096ba7f02f827cdce14840b794c60
Lois <Loisnek@yandex.ru>
2026-09-01 23:34:46 +0300

    Убрать дублирование кода для подсчёта длительности сна

--- src/main/java/ru/yandex/practicum/sleeptracker/AverageDurationAnalyzer.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/AverageDurationAnalyzer.java
@@ -8,10 +8,6 @@ import java.util.function.Function;
 public class AverageDurationAnalyzer implements Function<List<SleepSession>, SleepAnalysisResult> {
     @Override
     public SleepAnalysisResult apply(List<SleepSession> sessions) {
-        if (sessions.isEmpty()) {
-            return new SleepAnalysisResult("Средняя длительность сна (мин)", 0.0);
-        }
-
         double average = sessions.stream()
                 .mapToLong(SleepSession::getDurationMinutes)
                 .average()
